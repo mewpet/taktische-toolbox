b312357b5c9247576b5550eb08c03fd5dcacc8e7
mewpet <boos@petesso.de>
2023-04-07 04:45:27 +0200

    added navigation to new page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
-
+import { useNavigate } from "react-router-dom";
 
 // Components
 import Header from "../components/Header";
 
 
 const Home = () => {
+    let navigate = useNavigate();
+
     return (
         <div className="home">
             <Header />
@@ -14,7 +16,7 @@ const Home = () => {
                     <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit incidunt officia labore molestiae pariatur impedit repellendus, iure maiores quas voluptatum? Autem consequuntur, dolorum aperiam corporis rem inventore accusantium fugit exercitationem odio, molestias tempora quasi repellendus consequatur placeat ea, cupiditate obcaecati quia recusandae hic distinctio labore laudantium! Minus corporis fuga vero ullam reiciendis cupiditate quisquam nemo atque, animi iusto saepe laudantium!</div>
                 </div>
                 <div className="panel-grid">
-                    <div className="half-panel">
+                    <div onClick={() => navigate('/random-class-thursday')} className="half-panel">
                         <div className="panel-bg" />
                         <div className="title">Random Class Thursday</div>
                     </div>
