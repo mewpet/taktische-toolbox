30bd59dfa542c890a8352422c4915badd2584c69
mewpet <boos@petesso.de>
2023-04-07 07:44:57 +0200

    added dropdown

--- src/components/CollapsiblePanel.tsx
+++ src/components/CollapsiblePanel.tsx
@@ -3,8 +3,12 @@ import { useEffect, useRef, useState } from "react";
 //Assets
 import { CHEVRON_LEFT } from "../assets/exports";
 
+interface CollapsiblePanelProps {
+    title: string,
+    text: string
+}
 
-const CollapsiblePanel = () => {
+const CollapsiblePanel = (props: CollapsiblePanelProps) => {
     const [ open, setOpen ] = useState<boolean>(false);
     const [ showText, setShowText ] = useState<boolean>(false);
     const [ height, setHeight ] = useState<string>();
@@ -12,8 +16,8 @@ const CollapsiblePanel = () => {
     const contentRef = useRef(null);
     
     useEffect(() => {
-        console.log(open)
         if (contentRef && contentRef.current) {
+            console.log((contentRef.current as HTMLElement).getBoundingClientRect().height)
             setInitialHeight((contentRef.current as HTMLElement).clientHeight/10 + "rem")
             setHeight("0px")
         }
@@ -32,10 +36,6 @@ const CollapsiblePanel = () => {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
-    useEffect(() => {
-        console.log(open)
-    }, [open]);
-
     function animateCollapse() {
         if (open) {
             setShowText(false);
@@ -45,7 +45,7 @@ const CollapsiblePanel = () => {
             }, 200);
         }
         else {
-            setHeight(initialHeight)
+            setHeight("unset")
             setOpen(true)
             setTimeout(() => {
             setShowText(true);
@@ -56,13 +56,13 @@ const CollapsiblePanel = () => {
     return (
         <div onClick={() => animateCollapse()} className="collapsible-panel">
             <div className="visible-row">
-                <div className="title">How things work here!</div>
+                <div className="title">{props.title}</div>
                 <div className="button-container">
                     <img style={{transform: !open ? "rotate(-90deg)" : "rotate(90deg)"}} className="button" src={CHEVRON_LEFT} alt="" />
                 </div>
             </div>
-            <div style={{ height: height }} className="text-container">
-                <div ref={contentRef} style={{ opacity: showText ? "1" : "0"}} className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit incidunt officia labore molestiae pariatur impedit repellendus, iure maiores quas voluptatum? Autem consequuntur, dolorum aperiam corporis rem inventore accusantium fugit exercitationem odio, molestias tempora quasi repellendus consequatur placeat ea, cupiditate obcaecati quia recusandae hic distinctio labore laudantium! Minus corporis fuga vero ullam reiciendis cupiditate quisquam nemo atque, animi iusto saepe laudantium!</div>
+            <div style={{ height: height, marginTop: open ? "2rem" : "0px" }} className="text-container">
+                <div ref={contentRef} style={{ opacity: showText ? "1" : "0"}} className="text">{props.text}</div>
             </div>
         </div>
     )
--- src/pages/RCT.tsx
+++ src/pages/RCT.tsx
@@ -5,7 +5,7 @@ const RCT = () => {
     return (
         <div className="rct">
             <div className="content">
-                <CollapsiblePanel />
+                <CollapsiblePanel title="How to use the randomizer" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit incidunt officia labore molestiae pariatur impedit repellendus, iure maiores quas voluptatum? Autem consequuntur, dolorum aperiam corporis rem inventore accusantium fugit exercitationem odio, molestias tempora quasi repellendus consequatur placeat ea, cupiditate obcaecati quia recusandae hic distinctio labore laudantium! Minus corporis fuga vero ullam reiciendis cupiditate quisquam nemo atque, animi iusto saepe laudantium!" />
             </div>
         </div>
     )
